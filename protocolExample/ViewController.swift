7d19731decdef3d8539d9481be70877ccfda2c5f
Adv1sery <abdylda.sovetbekov@gmail.com>
2024-02-01 20:40:45 +0600

    Initial Commit

---
+++ protocolExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  protocolExample
+//
+//  Created by Abdylda Mamashev on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
